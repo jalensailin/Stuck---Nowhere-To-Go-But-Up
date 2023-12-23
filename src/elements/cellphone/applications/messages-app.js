3569ca78656f65c91479f0c6cbb01f991f779151
jalensailin <jalen.michalslevy@gmail.com>
2023-12-22 22:54:09 -0500

    messages app has better functionality; now user can select from a list of chat previews, similar to a iPhone

--- src/elements/cellphone/applications/messages-app.js
+++ src/elements/cellphone/applications/messages-app.js
@@ -6,9 +6,11 @@ export default class MessagesApp extends Application {
     super("messages", "messages-icon");
     this.initial.offset = vec2(90, 34);
 
-    this.currentChat = "friend1";
     this.messageMask = null;
     this.messageSpace = null;
+    this.previewSpace = null;
+
+    this.currentChat = "none";
     this.chats = {
       friend1: [
         {
@@ -31,15 +33,146 @@ export default class MessagesApp extends Application {
             "Yeah... I've just been feeling down lately. Haven't really been going out or doing much of anything",
           contact: false,
         },
+        {
+          message:
+            "Yeah... I've just been feeling down lately. Haven't really been going out or doing much of anything",
+          contact: false,
+        },
+        {
+          message:
+            "Yeah... I've just been feeling down lately. Haven't really been going out or doing much of anything",
+          contact: false,
+        },
+      ],
+      friend2: [
+        {
+          message: "Hey! Long time no talk. I miss uuu. How have things been?",
+          contact: true,
+        },
       ],
     };
   }
 
   /**
-   * Generate the game objects for a particular group of messages.
+   * Start this app.
+   * @override
    */
-  generateMessages() {
-    if (this.currentChat === "none") return;
+  async start() {
+    this.generateMessageMask();
+    if (this.currentChat === "none") {
+      this.generateMessagePreviews();
+    } else {
+      this.generateMessages();
+    }
+    await super.start();
+  }
+
+  /**
+   * Generate game objects for the messages.
+   * @override
+   */
+  async close() {
+    await super.close();
+    this.messageMask.destroy();
+    this.previewSpace = null;
+    this.messageMask = null;
+    this.messageSpace = null;
+  }
+
+  /**
+   * Fade messages in.
+   * @override
+   */
+  startAnimation() {
+    super.startAnimation();
+    const currentObject =
+      this.currentChat === "none" ? this.previewSpace : this.messageSpace;
+    return Animations.FadeChildren(currentObject, 0, 0.85, 0.3);
+  }
+
+  /**
+   * Fade messages out.
+   * @override
+   */
+  async closeAnimation() {
+    const currentObject =
+      this.currentChat === "none" ? this.previewSpace : this.messageSpace;
+    await Animations.FadeChildren(currentObject, 0.85, 0, 0.3);
+    return super.closeAnimation();
+  }
+
+  /**
+   * Set scroll behavior and other listeners.
+   * @override
+   */
+  setApplicationListeners() {
+    if (this.currentChat === "none") {
+      this.setPreviewsListeners();
+      return;
+    }
+    this.setChatListeners();
+  }
+
+  /**
+   * Set Chat listeners. These get destroyed when the messageSpace is destroyed.
+   */
+  setChatListeners() {
+    // Set chat scroll listeners.
+    const { messageSpace } = this;
+    messageSpace.onScroll((vector) => {
+      if (
+        vector.y > 0 &&
+        -messageSpace.totalHeight -
+          (messageSpace.pos.y - this.messageMask.height) <=
+          this.messageMask.height
+      )
+        return;
+      if (vector.y < 0 && messageSpace.pos.y <= this.messageMask.height) return; // Don't scroll below most recent message.
+      messageSpace.pos.y += vector.y / 2;
+    });
+
+    // Return to chat previews.
+    messageSpace.onMousePress("right", async () => {
+      this.currentChat = "none";
+      this.generateMessagePreviews();
+      // Fade the message-space out.
+      Animations.FadeChildren(this.messageSpace, 0.85, 0, 0.3);
+      // Destroy the preview space and set to null;
+      this.messageSpace.destroy();
+      this.messageSpace = null;
+      // Fade previews in.
+      await Animations.FadeChildren(this.previewSpace, 0, 0.85, 0.3);
+      this.setPreviewsListeners();
+    });
+  }
+
+  /**
+   * Set Chat Previews listeners. These get destroyed when the previewSpace is destroyed.
+   */
+  setPreviewsListeners() {
+    const chatPreviews = get("chatPreview", { recursive: true });
+    chatPreviews.forEach((preview) =>
+      preview.onClick(() => {
+        this.currentChat = preview.contact; // Update the current chat.
+        // Generate the messages based on the current chat.
+        this.generateMessages();
+        // Fade the preview-space out.
+        Animations.FadeChildren(this.previewSpace, 0.85, 0, 0.3);
+        // Destroy the preview space and set to null;
+        this.previewSpace.destroy();
+        this.previewSpace = null;
+        // Fade messages in.
+        Animations.FadeChildren(this.messageSpace, 0, 0.85, 0.3);
+        this.setChatListeners();
+      }),
+    );
+  }
+
+  /**
+   * Generate the mask which will contain all other game objects in this app,
+   * and keep them confined to the correct space.
+   */
+  generateMessageMask() {
     const { Cellphone } = StuckGame;
     // Message mask defines the area on the screen that our messages takes up.
     this.messageMask = Cellphone.screenSpace.add([
@@ -53,6 +186,95 @@ export default class MessagesApp extends Application {
       "messageMask",
       { name: "messageMask" },
     ]);
+  }
+
+  /**
+   * Generate the game objects which show
+   */
+  generateMessagePreviews() {
+    const { Cellphone } = StuckGame;
+    this.previewSpace = this.messageMask.add([pos(0, 0)]);
+
+    // Add Messages bar
+    const msgBar = this.previewSpace.add([
+      rect(Cellphone.screenSpace.width, 25, { fill: false }),
+      opacity(0),
+      timer(),
+    ]);
+    // Header text
+    msgBar.add([
+      pos(msgBar.width / 2, msgBar.height / 2),
+      text("Messages", { size: 16 }),
+      color(0, 0, 0),
+      anchor("center"),
+      opacity(0),
+      timer(),
+    ]);
+    // Horizontal rule.
+    msgBar.add([
+      pos(0, msgBar.height),
+      rect(Cellphone.screenSpace.width, 1),
+      opacity(0),
+      timer(),
+    ]);
+
+    // Iterate through each chat/contact and create spaces for them.
+    const chatList = Object.entries(this.chats);
+    chatList.forEach(([contact, messages], index) => {
+      const positionY = msgBar.height + index * 50;
+      // Container holding each chat.
+      const chatPreview = this.previewSpace.add([
+        pos(0, positionY),
+        rect(Cellphone.screenSpace.width, 50, { fill: false }),
+        area(),
+        opacity(0),
+        timer(),
+        "chatPreview",
+        { contact },
+      ]);
+
+      // Contact's name
+      chatPreview.add([
+        pos(50, 5),
+        text(contact, { size: 15 }),
+        color(0, 0, 0),
+        opacity(0),
+        timer(),
+      ]);
+
+      // Display most recent message from a contact.
+      const recentMessage = messages
+        .toReversed()
+        .find((msg) => msg.contact).message;
+      chatPreview.add([
+        pos(55, 22),
+        text(recentMessage, {
+          size: 11,
+          width: Cellphone.screenSpace.width - 55,
+          height: 50 - 25,
+        }),
+        color(88, 88, 88),
+        opacity(0),
+        timer(),
+      ]);
+
+      // Horizontal rule
+      this.previewSpace.add([
+        pos(50, positionY + 50),
+        rect(Cellphone.screenSpace.width - 50, 1),
+        opacity(0),
+        timer(),
+      ]);
+    });
+  }
+
+  /**
+   * Generate the game objects for a particular group of messages.
+   */
+  generateMessages() {
+    if (this.currentChat === "none") return;
+
+    const { Cellphone } = StuckGame;
 
     // messageSpace contains each chat message and moves up/down on scroll.
     this.messageSpace = this.messageMask.add([
@@ -60,7 +282,6 @@ export default class MessagesApp extends Application {
       "messageSpace",
       { name: "messageSpace", totalHeight: 0 },
     ]);
-
     // List of messages to display.
     const messageList = this.chats[this.currentChat].toReversed();
 
@@ -107,64 +328,5 @@ export default class MessagesApp extends Application {
       ]);
     });
     this.messageSpace.totalHeight = positionY;
-  }
-
-  /**
-   * Generate game objects for the messages.
-   * @override
-   */
-  async start() {
-    this.generateMessages();
-    await super.start();
-  }
-
-  /**
-   * Generate game objects for the messages.
-   * @override
-   */
-  async close() {
-    await super.close();
-    this.messageMask.destroy();
-    this.messageSpace.destroy();
-    this.messageMask = null;
-    this.messageSpace = null;
-  }
-
-  /**
-   * Fade messages in.
-   * @override
-   */
-  startAnimation() {
-    super.startAnimation();
-    return Animations.FadeChildren(this.messageSpace, 0, 1, 0.3);
-  }
-
-  /**
-   * Fade messages out.
-   * @override
-   */
-  async closeAnimation() {
-    await Animations.FadeChildren(this.messageSpace, 1, 0, 0.3);
-    return super.closeAnimation();
-  }
-
-  /**
-   * Set scroll behavior and other listeners.
-   * @override
-   */
-  setApplicationListeners() {
-    const { messageSpace } = this;
-    const scrollListener = messageSpace.onScroll((vector) => {
-      if (
-        vector.y > 0 &&
-        -messageSpace.totalHeight -
-          (messageSpace.pos.y - this.messageMask.height) <=
-          this.messageMask.height
-      )
-        return;
-      if (vector.y < 0 && messageSpace.pos.y <= this.messageMask.height) return; // Don't scroll below most recent message.
-      messageSpace.pos.y += vector.y;
-    });
-    this.listeners.push(scrollListener);
   }
 }
