17fc9b38685aa17dfcbde1b84bccb8e893ca2b7b
jalensailin <jalen.michalslevy@gmail.com>
2023-12-28 00:24:00 -0500

    break out snake start logic to its own function; now we can create new games after we game-over; add score keeping mechanic (no UI yet); add comments to snake functions;

--- src/elements/cellphone/applications/snake-app.js
+++ src/elements/cellphone/applications/snake-app.js
@@ -9,16 +9,16 @@ export default class SnakeApp extends Application {
     this.gameSpace = null;
     this.innerWindow = null;
     this.snakeHead = null;
-    this.gameOver = true;
 
     this.dir = DOWN;
+
+    this.gameOver = true;
+    this.score = 0;
+    this.highScore = 0;
   }
 
   async start() {
     this.initializeGameSpace();
-    this.initializeSnake();
-    this.generateApple();
-
     await super.start();
   }
 
@@ -28,7 +28,7 @@ export default class SnakeApp extends Application {
     this.gameSpace = null;
     this.innerWindow = null;
     this.snakeHead = null;
-    this.gameOver = true;
+    this.resetStates();
   }
 
   /**
@@ -50,42 +50,50 @@ export default class SnakeApp extends Application {
   }
 
   setApplicationListeners() {
-    const [snakeHead] = this.gameSpace.get("snakeHead", { recursive: true });
-    const [innerWindow] = this.gameSpace.get("innerWindow", {
-      recursive: true,
-    });
-    onKeyPress("enter", () => {
-      this.startSnakeGame();
-    });
+    this.listeners.push(
+      onKeyPress("enter", () => {
+        this.startSnakeGame();
+      }),
+    );
+  }
 
+  setSnakeListeners() {
+    // Set the movement controls
     this.setSnakeMovement();
 
-    snakeHead.onCollide("snakeBody", () => {
-      snakeHead.destroy();
-      const bodyParts = innerWindow.get("snakeBody", { recursive: true });
-      bodyParts.forEach((part) => part.destroy());
-      this.dir = DOWN;
-      this.gameOver = true;
+    // Game Over if we collide with our own body.
+    this.snakeHead.onCollide("snakeBody", () => {
+      this.snakeGameOver();
     });
 
-    snakeHead.onCollideUpdate("snakeBoundary", (_, collision) => {
+    // Game Over if we collide with the boundary.
+    this.snakeHead.onCollideUpdate("snakeBoundary", (_, collision) => {
       if (!collision.displacement.isZero()) return;
-      snakeHead.destroy();
-      const bodyParts = innerWindow.get("snakeBody", { recursive: true });
-      bodyParts.forEach((part) => part.destroy());
-      this.dir = DOWN;
-      this.gameOver = true;
+      this.snakeGameOver();
     });
 
-    snakeHead.onCollide("apple", (apple) => {
+    // Destroy Apple and increase score.
+    this.snakeHead.onCollide("apple", (apple) => {
       apple.destroy();
-      const bodyParts = innerWindow.get("snakeBody", { recursive: true });
+
+      // Increase score.
+      this.score += apple.points;
+      // Set high score if applicable.
+
+      // Get body parts and identify the most distal (the caboose).
+      if (this.score > this.highScore) this.highScore = this.score;
+      const bodyParts = this.innerWindow.get("snakeBody", { recursive: true });
       const caboose = bodyParts.at(-1);
-      this.createSnakeBodyPart(caboose.pos, caboose.lastDir);
+      // Create a new body part at the position of the caboose.
+      this.createSnakeBodyPart(caboose.pos);
+      // Generate an apple in a random position.
       this.generateApple();
     });
   }
 
+  /**
+   * Set the movement controls for the snake.
+   */
   setSnakeMovement() {
     const movementControls = [
       onKeyPress("i", () => {
@@ -101,8 +109,12 @@ export default class SnakeApp extends Application {
         if (this.dir !== LEFT) this.dir = RIGHT;
       }),
     ];
+    this.listeners.push(...movementControls);
   }
 
+  /**
+   * Initialize the space that the game takes place in.
+   */
   initializeGameSpace() {
     const { Cellphone } = StuckGame;
     // Game Space defines the area on the screen that our game takes up.
@@ -119,7 +131,7 @@ export default class SnakeApp extends Application {
       timer(),
     ]);
 
-    // Add boundary line.
+    // Add boundary line. Also acts as a collider.
     const boundary = outerWindow.add([
       pos(9.5, 9.5),
       rect(outerWindow.width - 19, outerWindow.height - 19, { fill: false }),
@@ -141,6 +153,9 @@ export default class SnakeApp extends Application {
     ]);
   }
 
+  /**
+   * Initialize the snake at the beginning of each round.
+   */
   initializeSnake() {
     this.snakeHead = this.innerWindow.add([
       pos(100, 100),
@@ -163,10 +178,10 @@ export default class SnakeApp extends Application {
   }
 
   /**
-   *
+   * Create an individual body part.
    * @param {vec2} pos
    */
-  createSnakeBodyPart(position, direction = DOWN) {
+  createSnakeBodyPart(position) {
     this.innerWindow.add([
       pos(position),
       rect(10, 10),
@@ -176,51 +191,104 @@ export default class SnakeApp extends Application {
       timer(),
       area({ shape: new Rect(vec2(1, 1), 8, 8) }),
       "snakeBody",
-      { lastDir: direction },
     ]);
   }
 
+  /**
+   * Generate the apple.
+   */
   generateApple() {
     const { innerWindow } = this;
     const bodyParts = this.gameSpace.get("snakeBody", { recursive: true });
 
+    // Assign the apple a random position on the grid.
     let applePosition = vec2(randi(22) * 10, randi(22) * 10);
+
+    // If the position intersects with the position of the snake, find a new random position.
     // eslint-disable-next-line no-loop-func
     while (bodyParts.some((part) => part.pos.eq(applePosition))) {
       applePosition = vec2(randi(22) * 10, randi(22) * 10);
     }
 
+    const points = randi(20) === 0 ? 10 : 1;
+    const fillColor = points === 10 ? rgb(255, 215, 0) : rgb(196, 41, 41);
+    const outlineColor = points === 10 ? rgb(110, 99, 4) : rgb(4, 110, 55);
+
+    // Add the apple.
     innerWindow.add([
       pos(applePosition),
       rect(10, 10, { radius: 2 }),
+      color(fillColor),
+      outline(2, outlineColor),
       opacity(),
       timer(),
       area(),
+      { points },
       "apple",
     ]);
   }
 
+  /**
+   * Create an instance of the game itself, and set associated listeners.
+   * @returns {void}
+   */
   startSnakeGame() {
+    // Early return if we are not in a game-over state.
     if (!this.gameOver) return;
+
+    // Destroy any existing apples.
+    const [existingApple] = this.innerWindow.get("apple");
+    if (existingApple) existingApple.destroy();
+
+    // Initialize the snake and generate the apples.
+    this.initializeSnake();
+    this.generateApple();
+
+    // Set all listeners for playing Snake.
+    this.setSnakeListeners();
+
     const { snakeHead, innerWindow } = this;
-    this.gameOver = false;
+    this.gameOver = false; // Game-in-progress
+
+    // The main Snake gameplay loop.
     snakeHead.loop(0.2, () => {
       const bodyParts = innerWindow.get("snakeBody", { recursive: true });
-      const bodyData = bodyParts.map((part, index) => {
-        const position =
+      // Map body parts to their next location.
+      const nextPositions = bodyParts.map((part, index) => {
+        const nextPosition =
           index === 0
             ? snakeHead.pos.clone()
             : bodyParts[index - 1].pos.clone();
-        return {
-          position,
-        };
+        return nextPosition;
       });
+      // Move body parts to next location.
       bodyParts.forEach((part, index) => {
-        part.pos = bodyData[index].position;
+        part.pos = nextPositions[index];
       });
       if (snakeHead.lastDir.eq(this.dir.scale(-1))) return;
+      // Move head to next location.
       snakeHead.pos = snakeHead.pos.add(this.dir.scale(10));
+      // Update previous direction
       snakeHead.lastDir = this.dir;
     });
+  }
+
+  /**
+   * Handle game over actions. Destroy Snake and reset properties.
+   */
+  snakeGameOver() {
+    this.snakeHead.destroy();
+    const bodyParts = this.innerWindow.get("snakeBody", { recursive: true });
+    bodyParts.forEach((part) => part.destroy());
+    this.resetStates();
+  }
+
+  /**
+   * Reset basic states of this app for game-over and closing.
+   */
+  resetStates() {
+    this.dir = DOWN;
+    this.score = 0;
+    this.gameOver = true;
   }
 }
